947aa7935e7746730bbc9c4ce4446c91c29eea61
muhammedkunhi362 <86754766+muhammedkunhi362@users.noreply.github.com>
2025-10-23 15:21:28 +0530

    Add About page route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
 import Category from "./pages/Category";
 import Post from "./pages/Post";
 import Search from "./pages/Search";
@@ -21,6 +22,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
           <Route path="/category/:category" element={<Category />} />
           <Route path="/post/:slug" element={<Post />} />
           <Route path="/search" element={<Search />} />
