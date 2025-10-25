9a2bb5601a11b3058d55560027b893f2bc79381a
muhammedkunhi362 <86754766+muhammedkunhi362@users.noreply.github.com>
2025-10-25 07:31:26 +0530

    Add Subscribe route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// src/App.tsx
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -8,6 +9,7 @@ import About from "./pages/About";
 import Category from "./pages/Category";
 import Post from "./pages/Post";
 import Search from "./pages/Search";
+import Subscribe from "./pages/Subscribe";
 import Admin from "./pages/Admin";
 import AdminLogin from "./pages/AdminLogin";
 import NotFound from "./pages/NotFound";
@@ -26,6 +28,7 @@ const App = () => (
           <Route path="/category/:category" element={<Category />} />
           <Route path="/post/:slug" element={<Post />} />
           <Route path="/search" element={<Search />} />
+          <Route path="/subscribe" element={<Subscribe />} />
           <Route path="/admin" element={<Admin />} />
           <Route path="/admin/login" element={<AdminLogin />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
